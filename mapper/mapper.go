6ed60d4c947c2b0fd27ae02c450b9502d76ec59f
Kaley Fujii <91648255+kaleyfujii@users.noreply.github.com>
2021-10-31 22:26:24 -1000

    Update mapper.go

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -2,8 +2,63 @@ package mapper
 
 import (
 	"fmt"
+	"log"	// logging errors
+	"os"	// create and open file
+	"KSCD/parser"
 )
 
 func Mapper() {
+
+	/*** CREATE THE terraform.xml FILE ***/
+
+	outFile, errCreate := os.Create("terraform.xml")
+	// error creating file
+	if errCreate != nil {
+		log.Println("Error creating file.", errCreate)
+		os.Exit(1)
+	}
+	// keep open
+	defer outFile.Close()
+
+	/*** PRINT PARSED DATA TO THE terraform.xml FILE ***/
+
+	// iterates through matches and prints each output
+	for i := range parser.Outputs {
+		fmt.Fprintln(outFile, "Outputs:", parser.Outputs[i])
+	}
+	fmt.Fprintln(outFile)
+
+	fmt.Fprintln(outFile, "Resources:")
+	fmt.Fprintln(outFile)
 	
+	// iterate through the resources and prints
+	for i := 0; i < len(parser.T.Resources); i++ {
+		fmt.Fprintln(outFile, "\t", "Type:", parser.T.Resources[i].Type)
+		fmt.Fprintln(outFile, "\t", "Name:", parser.T.Resources[i].Name)
+		fmt.Fprintln(outFile, "\t", "Provider:", parser.T.Resources[i].Provider)
+
+		fmt.Fprintln(outFile, "\t", "Instances:")
+		fmt.Fprintln(outFile, "\t\t", "Attributes:")
+
+		// iterate through the instances and prints
+		for j := 0; j < len(parser.T.Resources[i].Instances); j++ {
+			if len(parser.T.Resources[i].Instances[j].Attributes.ID) > 0 {
+				fmt.Fprintln(outFile, "\t\t\t", "ID:", parser.T.Resources[i].Instances[j].Attributes.ID)
+			}
+			if len(parser.T.Resources[i].Instances[j].Attributes.Name) > 0 {
+				fmt.Fprintln(outFile, "\t\t\t", "Name:", parser.T.Resources[i].Instances[j].Attributes.Name)
+			}
+			if len(parser.T.Resources[i].Instances[j].Attributes.Project) > 0 {
+				fmt.Fprintln(outFile, "\t\t\t", "Project:", parser.T.Resources[i].Instances[j].Attributes.Project)
+			}
+
+			// iterate through dependencies and prints
+			for k := 0; k < len(parser.T.Resources[i].Instances[j].Dependencies); k++ {
+				fmt.Fprintln(outFile, "\t\t", "Dependencies:", parser.T.Resources[i].Instances[j].Dependencies[k])
+			}
+		}
+	}
+	
+	// close file
+	outFile.Close()
 }

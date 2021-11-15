9af7f1f50f86f5aca407757575112259eb98b933
Kaley Fujii <91648255+kaleyfujii@users.noreply.github.com>
2021-11-14 18:16:06 -1000

    Update mapper.go
    combining our code

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -2,9 +2,10 @@ package mapper
 
 import (
 	"fmt"
-	"log"			// logging errors
-	"os"			// create and open file
-	"KSCD/parser"	// parser.go
+	"log"				// logging errors
+	"os"				// create and open file
+//	"KSCD/parser"		// parser.go
+	"KSCD/libraries"	// parser.go
 
 	"github.com/beevik/etree"	// creating xml file (go get github.com/beevik/etree)
 )
@@ -41,29 +42,90 @@ func Mapper() {
 	mxCell.CreateAttr("id", "1")
 	mxCell.CreateAttr("parent", "0")
 
-	// beginning part of style attribute for GCP icons
-	gcpIconFormat := "sketch=0;html=1;fillColor=#5184F3;strokeColor=none;verticalAlign=top;labelPosition=center;verticalLabelPosition=bottom;align=center;spacingTop=-6;fontSize=11;fontStyle=1;fontColor=#999999;shape="
+/*********************************************************************************************************************************************************************************************************************************/
 
-	// generates GCP icons; does not work for cards yet
-	for i := 0; i < len(parser.T.Resources); i++ {
-		mxCell = root.CreateElement("mxCell")
-		mxCell.CreateAttr("id", fmt.Sprint(i + 2))
-		mxCell.CreateAttr("value", parser.T.Resources[i].Type)
+//	objectType := "NAT"
+	objectType := "Gateway"
+	t := libraries.Lookup(objectType)
+	gcpShape := libraries.ShapeLookup[objectType]
 
-		// some loop or something to determine the icon we need; possibly based off of parser.T.Resources[i].Type ?
-		gcpIconShape := "mxgraph.gcp2.hexIcon;prIcon=gpu"
+//	// generates GCP icons; does not work for cards yet
+//	for i := 0; i < len(parser.T.Resources); i++ {
 
-		mxCell.CreateAttr("style", fmt.Sprint(gcpIconFormat + gcpIconShape))
-		mxCell.CreateAttr("parent", fmt.Sprint(1))	// this seems to be set for GCP icons
-		mxCell.CreateAttr("vertex", fmt.Sprint(1))	// this seems to be set for GCP icons
+//		// lookup value for switch
+//		t := libraries.Lookup(fmt.Sprint(parser.T.Resources[i].Type))
 
-		mxGeometry := mxCell.CreateElement("mxGeometry")
-		mxGeometry.CreateAttr("x", fmt.Sprint(225 + (i * 200)))		// 
-		mxGeometry.CreateAttr("y", fmt.Sprint(550))
-		mxGeometry.CreateAttr("width", fmt.Sprint(66))		// size for GCP icons
-		mxGeometry.CreateAttr("height", fmt.Sprint(58.5))	// size for GCP icons
-		mxGeometry.CreateAttr("as", "geometry")
-	}
+		switch t {
+		
+		/* <<< GCP / SERVICE CARDS */
+
+		case 1:
+
+			mxCell = root.CreateElement("mxCell")
+//			mxCell.CreateAttr("id", fmt.Sprint(i + 2)) ////
+			mxCell.CreateAttr("id", fmt.Sprint(2))
+			mxCell.CreateAttr("value", "")
+			mxCell.CreateAttr("style", "strokeColor=#dddddd;shadow=1;strokeWidth=1;rounded=1;absoluteArcSize=1;arcSize=2;")
+			mxCell.CreateAttr("vertex", fmt.Sprint(1))
+			mxCell.CreateAttr("parent", fmt.Sprint(1))
+	
+			mxGeometry := mxCell.CreateElement("mxGeometry")
+			mxGeometry.CreateAttr("x", "360")			// ??
+			mxGeometry.CreateAttr("y", "120")			// ??
+			mxGeometry.CreateAttr("width", "150")		// ??
+			mxGeometry.CreateAttr("height", "75")		// ??
+			mxGeometry.CreateAttr("as", "geometry")
+
+			mxCell = root.CreateElement("mxCell")
+//			mxCell.CreateAttr("id", fmt.Sprint(i + 2)) ////
+			mxCell.CreateAttr("id", fmt.Sprint(3)) ////
+//			mxCell.CreateAttr("value", parser.T.Resources[i].Type)
+			mxCell.CreateAttr("value", objectType)
+			mxCell.CreateAttr("style", fmt.Sprint("sketch=0;dashed=0;connectable=0;html=1;fillColor=#757575;strokeColor=none;part=1;labelPosition=right;verticalLabelPosition=middle;align=left;verticalAlign=middle;spacingLeft=5;fontSize=12;" + gcpShape))
+			mxCell.CreateAttr("vertex", "1")	// this seems to be set for GCP icons
+//			mxCell.CreateAttr("parent", fmt.Sprint(i + 1)) ////
+			mxCell.CreateAttr("parent", fmt.Sprint(2)) ////
+	
+			mxGeometry = mxCell.CreateElement("mxGeometry")
+			mxGeometry.CreateAttr("y", "0.5")
+			mxGeometry.CreateAttr("width", "32")
+			mxGeometry.CreateAttr("height", "32")
+			mxGeometry.CreateAttr("relative", "1")
+			mxGeometry.CreateAttr("as", "geometry")
+
+			mxPoint := mxGeometry.CreateElement("mxPoint")
+			mxPoint.CreateAttr("x", "5")
+			mxPoint.CreateAttr("y", "-16")
+			mxPoint.CreateAttr("as", "offset")
+		
+		/* GCP / SERVICE CARDS >>> */
+
+		/* <<< GCP ICONS */
+
+		// case 2:
+		// 	mxCell = root.CreateElement("mxCell")
+		// 	mxCell.CreateAttr("id", fmt.Sprint(i + 2))
+		// 	mxCell.CreateAttr("value", parser.T.Resources[i].Type)
+		// 	mxCell.CreateAttr("style", fmt.Sprint("sketch=0;html=1;fillColor=#5184F3;strokeColor=none;verticalAlign=top;labelPosition=center;verticalLabelPosition=bottom;align=center;spacingTop=-6;fontSize=11;fontStyle=1;fontColor=#999999;" + gcpShape))
+		// 	mxCell.CreateAttr("vertex", fmt.Sprint(1))	// this seems to be set for GCP icons
+		// 	mxCell.CreateAttr("parent", fmt.Sprint(1))	// this seems to be set for GCP icons
+	
+		// 	mxGeometry := mxCell.CreateElement("mxGeometry")
+		// 	mxGeometry.CreateAttr("x", fmt.Sprint(225 + (i * 200)))		// 
+		// 	mxGeometry.CreateAttr("y", fmt.Sprint(550))
+		// 	mxGeometry.CreateAttr("width", fmt.Sprint(66))		// size for GCP icons
+		// 	mxGeometry.CreateAttr("height", fmt.Sprint(58.5))	// size for GCP icons
+		// 	mxGeometry.CreateAttr("as", "geometry")
+
+		/* GCP ICONS >>> */
+
+		// error case
+		default:
+			log.Println("Error: No match.", errCreate)
+			os.Exit(1)
+		}
+
+//	}
 
 	/*** PRINT TO THE terraform.drawio FILE ***/
 

46e203abf622787fd20afc0011808f224f59fb18
sisonsco98 <83383661+sisonsco98@users.noreply.github.com>
2022-01-26 20:45:14 -1000

    pushing current progress

--- validator/validator.go
+++ validator/validator.go
@@ -48,27 +48,47 @@ func Validator() {
 	fmt.Println(ArrowsOverlap)
 	fmt.Println(ArrowResized)
 
-	// Loops through Pizza to get the slices
-	for _, pizza := range mapper.Pizza {
-		fmt.Println(pizza)
+	// Goes through all the arrows existing
+	for _, arrow := range mapper.ArrowRelationships {
 
-		// Goes through all the arrows existing
-		for _, arrow := range mapper.ArrowRelationships {
+		// Loops through Pizza to get the slices
+		for _, pizza := range mapper.Pizza {
+			fmt.Println(pizza)
 
-			// Check if arrows overlap at all
+			// Check if arrows overlap boxes at all
 
 			// Check if arrow has already been resized
 
 			// If the arrow does overlap AND it has not run through this function yet, create it's bending array (NOTE: Functions below are being performed above due to need to go through pizza function)
-			if ArrowOverlap(arrow.XPosSource, arrow.YPosSource, arrow.XPosTarget, arrow.YPosTarget) == true && ArrowResize(arrow.ArrowID+1) == false {
+			if ArrowsOverlap == true && ArrowResized == false {
 
-				// Finds a Y Coordinate that is unoccupied
-				var newY int = FindFreeSpace() // Will be taken out, and newY set to y coordinate found below
-				for _, slice := range mapper.Pizza {
-					fmt.Println(slice)
+				var newX int
+				var newY int
+
+				// Finding an X or Y Coordinate that is unoccupied
+				// Checking first if we need to move along x or y axis
+				if arrow.YPosSource == arrow.YPosTarget {
+					newX = arrow.XPosSource + pizza.Width
+					newY = arrow.YPosSource
+
+					// Check if still overlaps
+					for _, slice := range mapper.Pizza {
+						fmt.Println(slice)
+
+					}
+
+				} else {
+					newY = arrow.YPosSource + pizza.Height
+					newX = arrow.XPosSource
+
+					// Check if still overlaps
+					for _, slice := range mapper.Pizza {
+						fmt.Println(slice)
+
+					}
 				}
 
-				// Writing of necessary XML code to create bend
+				// Writing of necessary XML code to create bends
 				path := fmt.Sprintf("/mxGraphModel/root/mxCell[%d]/mxGeometry", arrow.ArrowID+1)
 				arrowGeom := xml.FindElement(path)
 
@@ -76,11 +96,11 @@ func Validator() {
 				array.CreateAttr("as", "points")
 
 				mxPoint := array.CreateElement("mxPoint")
-				mxPoint.CreateAttr("x", fmt.Sprint(arrow.XPosSource))
+				mxPoint.CreateAttr("x", fmt.Sprint(newX))
 				mxPoint.CreateAttr("y", fmt.Sprint(newY))
 
 				mxPoint = array.CreateElement("mxPoint")
-				mxPoint.CreateAttr("x", fmt.Sprint(arrow.XPosTarget))
+				mxPoint.CreateAttr("x", fmt.Sprint(newX))
 				mxPoint.CreateAttr("y", fmt.Sprint(newY))
 			}
 		}
@@ -98,21 +118,4 @@ func Validator() {
 	xml.WriteToFile("terraform.drawio")
 	inFile.Close()
 
-}
-
-// Function will take in the arrow coordinates, then needs to loop through Pizza and find if arrow intersects (might not work as its own function due to [])
-func ArrowOverlap(XPosSource int, YPosSource int, XPosTarget int, YPosTarget int) bool {
-
-	return true
-}
-
-// Checks if the arrow has been resized before
-func ArrowResize(ArrowID int) bool {
-	return false
-}
-
-// Will check for a y coordinate that has a free space
-func FindFreeSpace() int {
-
-	return 15
 }

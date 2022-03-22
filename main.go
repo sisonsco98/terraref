5fa653f7c756176680c17380a88f84141f6f8cfd
ReesesLover123 <43626074+ReesesLover123@users.noreply.github.com>
2022-03-21 22:54:41 -1000

    +flag comments
    Comments to help with flags.

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 )
 
 func main() {
-
+	// Format -> Flag format, --argument, default value, help message.
 	fileLoc := flag.String("statefile", "terraform.tfstate", "Filename that we're parsing from." )
 
+	// Parse the flag arguments~!
 	flag.Parse()
 
+	// Return value fileLoc is actually the address of a string variable with the value I'm looking for. 
 	var fileLocation = *fileLoc
 
 	// run parser.go

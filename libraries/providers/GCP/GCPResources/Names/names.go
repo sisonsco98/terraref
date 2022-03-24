433897adb6d6d3203a3d38d2f9d3232becbe374a
kaleyfujii <kaleyf@hawaii.edu>
2022-03-23 21:41:29 -1000

    split GCP Resources

---
+++ libraries/providers/GCP/GCPResources/Names/names.go
@@ -0,0 +1,32 @@
+package Names
+
+var NameMap = map[string]string{
+
+	/*** GCP / PATHS ***/
+
+	"primary_path":            "Primary Path",
+	"optional_primary_path":   "Optional Primary Path",
+	"secondary_path":          "Secondary Path",
+	"optional_secondary_path": "Optional Secondary Path",
+	"success_status":          "Success Status",
+	"failure":                 "Failure Status",
+
+	/****************************************************************************************************/
+
+	/*** ??? ***/
+
+	"google_app_engine_application":  "App Engine",
+	"google_storage_bucket":          "Bucket",
+	"google_compute_firewall_policy": "Cloud Firewall Rules",
+	"google_sql_database":            "Cloud SQL",
+	"google_compute_instance":        "Compute Engine",
+	"google_compute_firewall":        "Firewall",
+	"google_api_gateway_api":         "Gateway",
+	"google_container_cluster":       "Kubernetes",
+
+	/****************************************************************************************************/
+
+	"google_compute_subnetwork": "Subnetwork",
+	"google_compute_network":    "Network",
+	"google_project":            "Service Project",
+}
